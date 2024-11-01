42dc6ee08b054e13136ce8abe9a98a210c558807
Tom Archer <tarcher@microsoft.com>
2024-10-31 19:03:52 -0700

    interim put

--- quickstart/101-data-factory/main.tf
+++ quickstart/101-data-factory/main.tf
@@ -15,6 +15,23 @@ resource "random_string" "azurerm_data_factory" {
   upper   = false
 }
 
+resource "random_string" "azurerm_storage_account" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = random_string.azurerm_storage_account.result
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+}
+
 resource "azurerm_data_factory" "example" {
   name                = random_string.azurerm_data_factory.result
   location            = azurerm_resource_group.rg.location
